4e57ba5b6b6d46cbdad89c6c0c4391b38d4ad0ae
Teddy <teddykucaba@yahoo.com>
2022-11-03 18:03:11 -0500

    board and tile

--- CSPSudokuSolver.py
+++ CSPSudokuSolver.py
@@ -2,14 +2,22 @@
 
 class Board:
     def __init__(self):
-        # 9x9 board
-        self.size = 9
+        # 9x9 board flattened
+        self.size = 81
 
         # none signifies empty space
         board = []
-        for x in range(self.row_count):
-            row = []
-            for y in range(self.col_count):
-                row.append(None)
-            board.append(row)
+        
         self.board = board
+    class Tile:
+        def __init__(self,x,y,block,entry):
+            self.x = x
+            self.y = y
+            self.block = block
+            self.entry = entry
+            self.domain = {1,2,3,4,5,6,7,8,9}
+            if entry == None:
+                self.domain.remove(entry)
+
+    def getByX(self,x):
+        self.board
